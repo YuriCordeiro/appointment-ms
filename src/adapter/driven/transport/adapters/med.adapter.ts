b2039091542f07b53a66a1420f3f7308344cf284
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2024-09-25 00:08:48 -0300

    Fix external integration

--- src/adapter/driven/transport/adapters/med.adapter.ts
+++ src/adapter/driven/transport/adapters/med.adapter.ts
@@ -10,7 +10,7 @@ export class MedAdapter implements IMedPort {
     constructor(private readonly httpService: HttpService) { }
 
     getDoctorById(doctorId: string): Promise<AxiosResponse<DoctorResponseDTO>> {
-        const externalURL: string = `http://localhost:3004/doctors/id/${doctorId}`;
+        const externalURL: string = `http://afac82f1a6b784cad8d099bc19767025-491367876.us-east-1.elb.amazonaws.com/doctors/id/${doctorId}`;
 
         return this.httpService.axiosRef.get(externalURL);
     }
