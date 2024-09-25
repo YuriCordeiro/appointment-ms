7293c71ab9c97f91f7cff7f64438d472fd0c7ec6
Jean Silva <jeans_bs@outlook.com>
2024-09-25 17:19:32 -0300

    fix: add try catch on axios request

--- src/adapter/driven/transport/adapters/med.adapter.ts
+++ src/adapter/driven/transport/adapters/med.adapter.ts
@@ -2,7 +2,7 @@ import { AxiosResponse } from "axios";
 import { DoctorResponseDTO } from "src/adapter/driver/dtos/doctor-response.dto";
 import { IMedPort } from "../ports/med.port";
 import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class MedAdapter implements IMedPort {
@@ -12,7 +12,25 @@ export class MedAdapter implements IMedPort {
     getDoctorById(doctorId: string): Promise<AxiosResponse<DoctorResponseDTO>> {
         const externalURL: string = `http://afac82f1a6b784cad8d099bc19767025-491367876.us-east-1.elb.amazonaws.com/doctors/id/${doctorId}`;
 
-        return this.httpService.axiosRef.get(externalURL);
+        return this.httpService.axiosRef.get(externalURL, {
+            headers: { 'Content-Type': 'application/json' },
+            proxy: false
+        })
+        .then((response) => {
+            Logger.log(`Response from ${externalURL}`);
+            Logger.log(response.data);
+            return response;
+        })
+        .catch((error) => {
+            Logger.error(`Error from ${externalURL}`);
+            if (error.response) {
+                Logger.error(`Error status: ${error.response.status}`);
+                Logger.error(`Error data: ${JSON.stringify(error.response.data)}`);
+            } else {
+                Logger.error(`Error message: ${error.message}`);
+            }
+            return null;
+        });        
     }
 
 }
